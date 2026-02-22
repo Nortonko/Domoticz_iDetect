25bff15bca51a3e1389bec01d74134ee844e37e0
Nortonko <rastislav@napismi.sk>
2026-02-22 19:36:03 +0100

    add support for newer UniFi cloud gateway as UCG to http unifi metod

--- trackers/http_unifi.py
+++ trackers/http_unifi.py
@@ -1,90 +1,134 @@
-# Tracker for Ubiquiti Unifi Controller
+# Tracker for Ubiquiti Unifi Controller (Updated for UniFi OS / Cloud Gateways)
 
 import Domoticz
 import requests
 import json
+import urllib3
 from trackers.tracker_base import tracker
+
+# Potlačenie varovania o neplatnom SSL certifikáte
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 class http_unifi(tracker):
-	def __init__(self, *args, **kwargs):
-		# Update default port
-		if self.tracker_port is None:
-			self.tracker_port = 443
+    def __init__(self, *args, **kwargs):
+        # 1. Najprv zavoláme super().__init__, aby sa vytvorili základné atribúty
+        super().__init__(*args, **kwargs)
 
-		super().__init__(*args, **kwargs)
+        # 2. Ttracker_port existuje (vytvoril ho super)
+        # Ak v nastaveniach Domoticz nie je port vyplnený, nastavíme 443
+        if (
+            not hasattr(self, "tracker_port")
+            or self.tracker_port is None
+            or self.tracker_port == ""
+        ):
+            self.tracker_port = 443
 
-		self.baseurl = 'https://{}:{}'.format(self.tracker_ip, self.tracker_port)
-		self.site = 'default'
-		self.verify_ssl = False
-		self.prepare_for_polling()
+        # 3. Zvyšok inicializácie
+        self.baseurl = "https://{}:{}".format(self.tracker_ip, self.tracker_port)
+        self.site = "default"
+        self.verify_ssl = False
+        self.is_unifi_os = False
+        self.prepare_for_polling()
 
-	def poll_present_tag_ids(self):
-		try:
-			if not self.http_session:
-				self.connect()
+    def poll_present_tag_ids(self):
+        try:
+            if not self.http_session:
+                if not self.connect():
+                    return
 
-			response = self.http_session.get(
-				"{}/api/s/{}/stat/sta".format(self.baseurl, self.site),
-				verify=self.verify_ssl, 
-				data="json={}"
-			)
-		except Exception as e:
-			Domoticz.Error(self.tracker_ip + ' Polling error: ' + str(e))
-			self.close_connection()
+            # Ak je to UniFi OS, prefix /proxy/network
+            prefix = "/proxy/network" if self.is_unifi_os else ""
+            url = "{}{}/api/s/{}/stat/sta".format(self.baseurl, prefix, self.site)
 
-		if (response.status_code == 401):
-			self.close_connection()
-			Domoticz.Error(self.tracker_ip + ' Invalid login, or login has expired')
-			return
+            response = self.http_session.get(url, verify=self.verify_ssl, timeout=10)
 
-		raw_data = response.text
-		Domoticz.Debug(self.tracker_ip + ' Returned: ' + raw_data)
+            if response.status_code == 401:
+                Domoticz.Debug(self.tracker_ip + " Session expired, reconnecting...")
+                self.close_connection()
+                return
 
-		self.receiver_callback(raw_data)
+            raw_data = response.text
+            Domoticz.Debug(self.tracker_ip + " Returned: " + raw_data)
+            self.receiver_callback(raw_data)
 
-	def connect(self):
-		login_data = {
-			'username': self.tracker_user,
-			'password': self.tracker_password
-		}
+        except Exception as e:
+            Domoticz.Error(self.tracker_ip + " Polling error: " + str(e))
+            self.close_connection()
 
-		self.http_session = requests.Session()
+    def connect(self):
+        login_data = {
+            "username": self.tracker_user,
+            "password": self.tracker_password,
+            "remember": True,
+        }
 
-		response = self.http_session.post(
-			"{}/api/login".format(self.baseurl),
-			data=json.dumps(login_data),
-			verify=self.verify_ssl
-		)
+        self.http_session = requests.Session()
 
-		if response.status_code == 400:
-			Domoticz.Error(self.tracker_ip + 'Failed to log in to Unifi API')
-			return False
+        # najprv UniFi OS prihlásenie (/api/auth/login)
+        login_url = "{}/api/auth/login".format(self.baseurl)
+        try:
+            response = self.http_session.post(
+                login_url, json=login_data, verify=self.verify_ssl, timeout=10
+            )
 
-		Domoticz.Status(self.baseurl + ' Initialized as Ubiquiti Unifi Controller API')
-		return True
+            if response.status_code == 200:
+                self.is_unifi_os = True
+                Domoticz.Status(
+                    self.tracker_ip + " Connected to UniFi OS (Cloud Gateway)"
+                )
+                return True
 
-	def disconnect(self):
-		try:
-			self.http_session.get("{}/logout".format(self.baseurl))
-		finally:
-			self.close_connection()
+            # 2. Ak 1. zlyhalo, starý spôsob (/api/login)
+            login_url_old = "{}/api/login".format(self.baseurl)
+            response = self.http_session.post(
+                login_url_old, json=login_data, verify=self.verify_ssl, timeout=10
+            )
 
-	def close_connection(self):
-		try:
-			self.http_session.close()
-			self.http_session = None
-			Domoticz.Debug(self.tracker_ip + ' HTTP session closed')
-		except Exception as e:
-			Domoticz.Debug(self.tracker_ip + ' Close session exception: ' + str(e))
-		return
-	   
-	def prepare_for_polling(self):
-		if self.connect():
-			self.is_ready = True
+            if response.status_code == 200:
+                self.is_unifi_os = False
+                Domoticz.Status(
+                    self.tracker_ip + " Connected to Legacy UniFi Controller"
+                )
+                return True
 
-	def stop_now(self):
-		self.is_ready = False
-		self.disconnect()
+            Domoticz.Error(
+                self.tracker_ip
+                + " Login failed (Status: {})".format(response.status_code)
+            )
+            return False
 
-		super().stop_now()
+        except Exception as e:
+            Domoticz.Error(
+                self.tracker_ip + " Connection error during login: " + str(e)
+            )
+            return False
+
+    def disconnect(self):
+        try:
+            logout_path = "/api/auth/logout" if self.is_unifi_os else "/logout"
+            self.http_session.get(
+                "{}{}".format(self.baseurl, logout_path), verify=self.verify_ssl
+            )
+        except:
+            pass
+        finally:
+            self.close_connection()
+
+    def close_connection(self):
+        try:
+            if self.http_session:
+                self.http_session.close()
+            self.http_session = None
+            Domoticz.Debug(self.tracker_ip + " HTTP session closed")
+        except Exception as e:
+            Domoticz.Debug(self.tracker_ip + " Close session exception: " + str(e))
+
+    def prepare_for_polling(self):
+        if self.connect():
+            self.is_ready = True
+
+    def stop_now(self):
+        self.is_ready = False
+        self.disconnect()
+        super().stop_now()
